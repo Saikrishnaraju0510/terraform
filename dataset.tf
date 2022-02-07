9b22e627e7297a1deb215829fb94ea2b2bdd2a41
Sai Krishna Raju Chekuri <75117530+Saikrishnaraju0510@users.noreply.github.com>
2022-02-07 17:02:05 +0530

    create dataset and service account in gcp

---
+++ dataset.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("key.json")
+  project = "modular-asset-332406"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = "demo-py@modular-asset-332406.iam.gserviceaccount.com"
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
