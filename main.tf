e80e73b68c9f309676ab394bbc606ab9a96c9cd8
Sai Krishna Raju Chekuri <75117530+Saikrishnaraju0510@users.noreply.github.com>
2022-02-15 17:02:05 +0530

    terraform

--- main.tf
+++ main.tf
@@ -13,21 +13,67 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
+  labels = {
+    env = "default"
   }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
+  access {
+    role          = "OWNER"
+    user_by_email = "demo-py@modular-asset-332406.iam.gserviceaccount.com"
   }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
+
+resource "google_bigquery_table" "cust_data"{
+  dataset_id="example_dataset"
+  table_id = "cust_data"
+
+  external_data_configuration {
+    autodetect=true
+    source_format="CSV"
+    csv_options {
+      quote = ""
+      field_delimiter = ","
+      skip_leading_rows=1
+    }
+
+    
+    source_uris=["gs://us-central1-c1-dea909ea-bucket/supermarket_sales.csv"]
+  }
+}
+
+resource "google_bigquery_table" "view"{
+  dataset_id="example_dataset"
+  table_id = "view2"
+  project = "modular-asset-332406"
+  view {
+    query =  "SELECT * FROM `modular-asset-332406.example_dataset.cust_data` WHERE Quantity>4"
+    use_legacy_sql = false
+  }
+  lifecycle {
+    ignore_changes = [
+      encryption_configuration
+    ]
+  }
+}
+
+resource "google_storage_bucket_object" "picture" {
+  name   = "butterfly01"
+  source = "C:/Users/gowth/learn-terraform-gcp/supermarket_sales.csv"
+  bucket = "sai_1999"
 }
